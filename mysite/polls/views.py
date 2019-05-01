207ebfd533fb17621780fd99c4bff587159c1294
JosephJMurphy <Jiggypig.The.Elf@gmail.com>
2019-05-01 14:29:14 +0100

    New Wikipage First commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-    title = 'Hello world'
+    title = "Hello world"
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
