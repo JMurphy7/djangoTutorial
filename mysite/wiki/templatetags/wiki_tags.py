cebf1cb3580cb0d4ba6de3d40e70299f7fd6de86
JosephJMurphy <Jiggypig.The.Elf@gmail.com>
2019-05-08 16:17:02 +0100

    Minor tweaks to creation page. Finished wiki tags installation

--- mysite/wiki/templatetags/wiki_tags.py
+++ mysite/wiki/templatetags/wiki_tags.py
@@ -1,10 +1,16 @@
 from django import template
-import markdown
+import markdown, re
 
+
+wikiWordV2 = re.compile(r"\[\[([A-Za-z0-9_]+)\]\]")
 register = template.Library()
 
 @register.filter
 def markup(text):
-    f = markdown.markdown(text)
-    print(f)
-    return f
+    return markdown.markdown(text)
+
+@register.filter
+def wikify(text):
+    return wikiWordV2.sub(r'''
+    <a href="/wiki/\1/">\1</a>
+    ''', text)
